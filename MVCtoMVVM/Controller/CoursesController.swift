10aa847075d5e9cf43015961ab12dbb5857a80c7
coras-company-code <cora-jane.meade@cognizant.com>
2022-03-09 10:27:04 +0000

    Files and folders for MVC

--- MVCtoMVVM/Controller/CoursesController.swift
+++ MVCtoMVVM/Controller/CoursesController.swift
@@ -9,43 +9,46 @@ import Foundation
 import UIKit
 
 class CoursesController: UITableViewController {
-    
-    var courseViewModels = [CourseViewModel]()
+
+    var courses = [Course]()
     let cellId = "cellId"
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupNavBar()
         setupTableView()
         fetchData()
     }
-    
+
     fileprivate func fetchData() {
         Service.shared.fetchCourses { (courses, err) in
             if let err = err {
                 print("Failed to fetch courses:", err)
                 return
             }
-            
-            self.courseViewModels = courses?.map({return CourseViewModel(course: $0)}) ?? []
+
+            self.courses = courses ?? []
             self.tableView.reloadData()
+
+
         }
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return courseViewModels.count
+        return courses.count
+
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! CourseCell
+        let course = courses[indexPath.row]
+        cell.course = course //unsure
 
-        let courseViewModel = courseViewModels[indexPath.row]
-        cell.courseViewModel = courseViewModel
-        
+
         return cell
     }
-    
+
     fileprivate func setupTableView() {
         tableView.register(CourseCell.self, forCellReuseIdentifier: cellId)
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
@@ -55,16 +58,16 @@ class CoursesController: UITableViewController {
         tableView.estimatedRowHeight = 50
         tableView.tableFooterView = UIView()
     }
-    
+
     fileprivate func setupNavBar() {
         navigationItem.title = "Courses"
         navigationController?.navigationBar.prefersLargeTitles = true
-//        navigationController?.navigationBar.backgroundColor = .yellow
-        navigationController?.navigationBar.isTranslucent = true
-//        navigationController?.navigationBar.barTintColor = UIColor.rgb(r: 50, g: 199, b: 242)
+        //navigationController?.navigationBar.backgroundColor = .yellow
+        navigationController?.navigationBar.isTranslucent = false
+       // navigationController?.navigationBar.barTintColor = UIColor.rgb(r: 50, g: 199, b: 242)
         navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
     }
-    
+
 }
 
 class CustomNavigationController: UINavigationController {
@@ -76,9 +79,10 @@ class CustomNavigationController: UINavigationController {
 extension UIColor {
     static let mainTextBlue = UIColor.rgb(r: 7, g: 71, b: 89)
     static let highlightColor = UIColor.rgb(r: 50, g: 199, b: 242)
-    
+
     static func rgb(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
         return UIColor(red: r/255, green: g/255, blue: b/255, alpha: 1)
     }
 }
+
 
--- MVCtoMVVM/View/CourseCell.swift
+++ MVCtoMVVM/View/CourseCell.swift
@@ -8,39 +8,38 @@
 import Foundation
 
 import UIKit
-
+import Foundation
 class CourseCell: UITableViewCell {
+    var course: Course! {
+            didSet {
+                textLabel?.text = course.name
     
-    var courseViewModel: CourseViewModel! {
-        didSet {
-            textLabel?.text = courseViewModel.name
-            detailTextLabel?.text = courseViewModel.detailTextString
-            accessoryType = courseViewModel.accessoryType
+                if course.numberOfLessons > 35 {
+                    accessoryType = .detailDisclosureButton
+                    detailTextLabel?.text = "Lessons 30+ Check it Out"
+                } else {
+                    detailTextLabel?.text = "Lessons: \(course.numberOfLessons)"
+                    accessoryType = .none
+                }
+            }
         }
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        contentView.backgroundColor = isHighlighted ? .highlightColor : .white
+        textLabel?.textColor = isHighlighted ? UIColor.white : .mainTextBlue
+        detailTextLabel?.textColor = isHighlighted ? .white : .black
     }
-    
-    //Extra UI design
-//    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
-//        super.setHighlighted(highlighted, animated: animated)
-//        contentView.backgroundColor = isHighlighted ? .highlightColor : .white
-//        textLabel?.textColor = isHighlighted ? UIColor.white : .mainTextBlue
-//        detailTextLabel?.textColor = isHighlighted ? .white : .black
-//    }
-//
-//
-    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    
         // cell customization
-                textLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-                textLabel?.numberOfLines = 0
-                detailTextLabel?.textColor = .black
-                detailTextLabel?.font = UIFont.systemFont(ofSize: 20, weight: .light)
-           
+        textLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        textLabel?.numberOfLines = 0
+        detailTextLabel?.textColor = .black
+        detailTextLabel?.font = UIFont.systemFont(ofSize: 20, weight: .light)
     }
-        
-
     
     required init?(coder aDecoder: NSCoder) {
         fatalError()
