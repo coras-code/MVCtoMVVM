f03893da4c136e7bc0c7c75a06d21a8a735a1286
coras-company-code <cora-jane.meade@cognizant.com>
2022-03-23 15:30:03 +0000

    converted from MVC to MVVM

--- MVCtoMVVM/Controller/BirdsController.swift
+++ MVCtoMVVM/Controller/BirdsController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class CoursesController: UITableViewController { //Info: A subclass of UIViewController, already has the properties and protocols (delegate and datasource that you need to create a tableview)
     
-    var birds = [Bird]()
+    var birdsViewModels = [BirdViewModel]()
     let cellId = "cellId"
     
     override func viewDidLoad() {
@@ -24,32 +24,25 @@ class CoursesController: UITableViewController { //Info: A subclass of UIViewCon
                return
            }
 
-           self.birds = birds ?? [] //Additional Research: do i need ?? []
+        self.birdsViewModels = birds?.map({return BirdViewModel(bird: $0)}) ?? [] //Additional Research: do i need ?? []
            self.tableView.reloadData()
 
        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return birds.count
+        return birdsViewModels.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        let bird = birds[indexPath.row]
+        let bird = birdsViewModels[indexPath.row]
         
-        cell.textLabel?.text = bird.comName
-        if bird.howMany != nil {
-            if bird.howMany! > 10 {
-                cell.accessoryType = .detailDisclosureButton
-                cell.detailTextLabel?.text = "10+ birds sighted here"
-            } else {
-                cell.detailTextLabel?.text = "\(bird.howMany!) birds sighted at \(bird.locName)"
-                cell.accessoryType = .none
-            }
-        }
-        
+        cell.textLabel?.text = bird.name
+        cell.accessoryType = bird.accessoryType
+        cell.detailTextLabel?.text = bird.birdsSighted
+    
         cell.selectionStyle = .none
         
         //this does not need to be extracted out as this is to be able to programmatically/create a nib to design a (custom) cell but as we are using a storyboard we dont need to do this
---
+++ MVCtoMVVM/ViewModel/BirdViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ModelView.swift
+//  MVCtoMVVM
+//
+//  Created by M_931521 on 23/03/2022.
+//
+
+import Foundation
+import UIKit
+
+struct BirdViewModel {
+    let name: String
+    let birdsSighted: String
+    let accessoryType:  UITableViewCell.AccessoryType
+
+    //DI
+    init(bird : Bird){
+        self.name = bird.comName
+
+        if bird.howMany != nil {
+            if bird.howMany! > 5 {
+                self.birdsSighted = "More than 5 birds sighted here!"
+                self.accessoryType = .detailDisclosureButton
+                
+            } else {
+                self.birdsSighted =  "\(bird.howMany!) birds sighted at \(bird.locName)"
+                self.accessoryType = .none
+                
+            }
+        } else {
+            self.birdsSighted = "Unknown how many birds where sighted here"
+            self.accessoryType = .none
+        }
+        
+        
+        
+    }
+}
+
